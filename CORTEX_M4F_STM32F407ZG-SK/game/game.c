e13d93f082827a44d568606de7929b21dcf27b05
steven_ux <steven@ux.com.tw>
2014-08-16 17:38:40 +0800

    game.c: retab

--- CORTEX_M4F_STM32F407ZG-SK/game/game.c
+++ CORTEX_M4F_STM32F407ZG-SK/game/game.c
@@ -36,274 +36,274 @@ uint8_t demoMode = 0;
 void
 BallReset()
 {
-	ballX = ( LCD_PIXEL_WIDTH - 5 ) / 2;
-	ballY = ( LCD_PIXEL_HEIGHT - 5 ) / 2;
+    ballX = ( LCD_PIXEL_WIDTH - 5 ) / 2;
+    ballY = ( LCD_PIXEL_HEIGHT - 5 ) / 2;
 
-	ballVX = 5;
-	ballVY = 5;
+    ballVX = 5;
+    ballVY = 5;
 
-	ballIsRun = 1;
+    ballIsRun = 1;
 }
 
 void
 GAME_EventHandler1()
 {
-	if( STM_EVAL_PBGetState( BUTTON_USER ) ){
+    if( STM_EVAL_PBGetState( BUTTON_USER ) ){
 
-		player1IsReversed = 0;
+        player1IsReversed = 0;
 
-		while( STM_EVAL_PBGetState( BUTTON_USER ) );
+        while( STM_EVAL_PBGetState( BUTTON_USER ) );
 
-		player1IsReversed = 1;
-	}
+        player1IsReversed = 1;
+    }
 }
 
 void
 GAME_EventHandler2()
 {
-	if( IOE_TP_GetState()->TouchDetected ){
+    if( IOE_TP_GetState()->TouchDetected ){
 
-		player2IsReversed = 1;
+        player2IsReversed = 1;
 
-		while( IOE_TP_GetState()->TouchDetected );
+        while( IOE_TP_GetState()->TouchDetected );
 
-		player2IsReversed = 0;
-	}
+        player2IsReversed = 0;
+    }
 }
 
 void
 GAME_EventHandler3()
 {
-	if( ballIsRun == 0 ){
-		BallReset();
-	}
+    if( ballIsRun == 0 ){
+        BallReset();
+    }
 }
 
 void
 GAME_Update()
 {
-	//Player1
-	LCD_SetTextColor( LCD_COLOR_BLACK );
-	LCD_DrawFullRect( player1X, player1Y, player1W, player1H );
-	LCD_DrawFullRect( player2X, player2Y, player2W, player2H );
+    //Player1
+    LCD_SetTextColor( LCD_COLOR_BLACK );
+    LCD_DrawFullRect( player1X, player1Y, player1W, player1H );
+    LCD_DrawFullRect( player2X, player2Y, player2W, player2H );
 
-	if( demoMode == 0 ){
+    if( demoMode == 0 ){
 
-		if( player1IsReversed )
-			player1X -= 5;
-		else
-			player1X += 5;
+        if( player1IsReversed )
+            player1X -= 5;
+        else
+            player1X += 5;
 
-		if( player1X <= 0 )
-			player1X = 0;
-		else if( player1X + player1W >= LCD_PIXEL_WIDTH )
-			player1X = LCD_PIXEL_WIDTH - player1W;
+        if( player1X <= 0 )
+            player1X = 0;
+        else if( player1X + player1W >= LCD_PIXEL_WIDTH )
+            player1X = LCD_PIXEL_WIDTH - player1W;
 
-		//Player2
-		if( player2IsReversed )
-			player2X -= 5;
-		else
-			player2X += 5;
+        //Player2
+        if( player2IsReversed )
+            player2X -= 5;
+        else
+            player2X += 5;
 
-		if( player2X <= 0 )
-			player2X = 0;
-		else if( player2X + player2W >= LCD_PIXEL_WIDTH )
-			player2X = LCD_PIXEL_WIDTH - player2W;
+        if( player2X <= 0 )
+            player2X = 0;
+        else if( player2X + player2W >= LCD_PIXEL_WIDTH )
+            player2X = LCD_PIXEL_WIDTH - player2W;
 
-		//Ball
-		if( ballIsRun == 1 ){
+        //Ball
+        if( ballIsRun == 1 ){
 
-			LCD_SetTextColor( LCD_COLOR_BLACK );
-			LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
+            LCD_SetTextColor( LCD_COLOR_BLACK );
+            LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
 
-			//Touch wall
-			ballX += ballVX;
-			if( ballX <= 0 ){
-				ballX = 0;
-				ballVX *= -1;
-			}
-			else if( ballX + ballSize >= LCD_PIXEL_WIDTH ){
-				ballX = LCD_PIXEL_WIDTH - ballSize;
-				ballVX *= -1;
-			}
+            //Touch wall
+            ballX += ballVX;
+            if( ballX <= 0 ){
+                ballX = 0;
+                ballVX *= -1;
+            }
+            else if( ballX + ballSize >= LCD_PIXEL_WIDTH ){
+                ballX = LCD_PIXEL_WIDTH - ballSize;
+                ballVX *= -1;
+            }
 
-			//PONG!
-			ballY += ballVY;
-			if( ballY + ballSize >= player2Y ){
-				if( ballX + ballSize >= player2X && ballX <= player2X + player2W ){
-					if( ballX - ballSize <= player2Y + player2W/4 ){
-						ballVY =-3;
-						ballVX =-7;
-					}
-					else if( ballX >= player2Y + player2W - player2W/4 ){
-						ballVY =-3;
-						ballVX = 7;
-					}
-					else if( ballX + ballSize < player2Y + player2W/2 ){
-						ballVY =-7;
-						ballVX =-3;
-					}
-					else if( ballX > player2Y + player2W/2 ){
-						ballVY =-7;
-						ballVX = 3;
-					}
-					else{
-						ballVY =-9;
-						ballVX = 0;
-					}
-				}
-				else
-					BallReset();
-			}
+            //PONG!
+            ballY += ballVY;
+            if( ballY + ballSize >= player2Y ){
+                if( ballX + ballSize >= player2X && ballX <= player2X + player2W ){
+                    if( ballX - ballSize <= player2Y + player2W/4 ){
+                        ballVY =-3;
+                        ballVX =-7;
+                    }
+                    else if( ballX >= player2Y + player2W - player2W/4 ){
+                        ballVY =-3;
+                        ballVX = 7;
+                    }
+                    else if( ballX + ballSize < player2Y + player2W/2 ){
+                        ballVY =-7;
+                        ballVX =-3;
+                    }
+                    else if( ballX > player2Y + player2W/2 ){
+                        ballVY =-7;
+                        ballVX = 3;
+                    }
+                    else{
+                        ballVY =-9;
+                        ballVX = 0;
+                    }
+                }
+                else
+                    BallReset();
+            }
 
-			if( ballY <= player1Y + player1H ){
-					if( ballX + ballSize >= player1X && ballX <= player1X + player1W ){
-						if( ballX - ballSize <= player1Y + player1W/4 ){
-							ballVY = 3;
-							ballVX =-7;
-						}
-						else if( ballX >= player1Y + player1W - player1W/4 ){
-							ballVY = 3;
-							ballVX = 7;
-						}
-						else if( ballX + ballSize < player1Y + player1W/2 ){
-							ballVY = 7;
-							ballVX =-3;
-						}
-						else if( ballX > player1Y + player1W/2 ){
-							ballVY = 7;
-							ballVX = 3;
-						}
-						else{
-							ballVY = 9;
-							ballVX = 0;
-						}
-					}
-					else
-						BallReset();
-				}
-			}
-		}
-		else{	//if demoMode == 1
+            if( ballY <= player1Y + player1H ){
+                    if( ballX + ballSize >= player1X && ballX <= player1X + player1W ){
+                        if( ballX - ballSize <= player1Y + player1W/4 ){
+                            ballVY = 3;
+                            ballVX =-7;
+                        }
+                        else if( ballX >= player1Y + player1W - player1W/4 ){
+                            ballVY = 3;
+                            ballVX = 7;
+                        }
+                        else if( ballX + ballSize < player1Y + player1W/2 ){
+                            ballVY = 7;
+                            ballVX =-3;
+                        }
+                        else if( ballX > player1Y + player1W/2 ){
+                            ballVY = 7;
+                            ballVX = 3;
+                        }
+                        else{
+                            ballVY = 9;
+                            ballVX = 0;
+                        }
+                    }
+                    else
+                        BallReset();
+                }
+            }
+        }
+        else{   //if demoMode == 1
 
-			//Player1 move
-			if( ballVY < 0 ){
-				if( player1X + player1W/2 < ballX + ballSize/2 ){
-					player1X += 8;
-					player2X += 2;
-				}
-				else{
-					player1X -= 8;
-					player2X -= 2;
-				}
-			}
+            //Player1 move
+            if( ballVY < 0 ){
+                if( player1X + player1W/2 < ballX + ballSize/2 ){
+                    player1X += 8;
+                    player2X += 2;
+                }
+                else{
+                    player1X -= 8;
+                    player2X -= 2;
+                }
+            }
 
-			//Player2 move
-			if( ballVY > 0 ){
-				if( player2X + player2W/2 < ballX + ballSize/2 ){
-					player1X += 2;
-					player2X += 8;
-				}
-				else{
-					player1X -= 2;
-					player2X -= 8;
-				}
+            //Player2 move
+            if( ballVY > 0 ){
+                if( player2X + player2W/2 < ballX + ballSize/2 ){
+                    player1X += 2;
+                    player2X += 8;
+                }
+                else{
+                    player1X -= 2;
+                    player2X -= 8;
+                }
 
-			}
+            }
 
-			if( player1X <= 0 )
-				player1X = 0;
-			else if( player1X + player1W >= LCD_PIXEL_WIDTH )
-				player1X = LCD_PIXEL_WIDTH - player1W;
+            if( player1X <= 0 )
+                player1X = 0;
+            else if( player1X + player1W >= LCD_PIXEL_WIDTH )
+                player1X = LCD_PIXEL_WIDTH - player1W;
 
-			if( player2X <= 0 )
-				player2X = 0;
-			else if( player2X + player2W >= LCD_PIXEL_WIDTH )
-				player2X = LCD_PIXEL_WIDTH - player2W;
+            if( player2X <= 0 )
+                player2X = 0;
+            else if( player2X + player2W >= LCD_PIXEL_WIDTH )
+                player2X = LCD_PIXEL_WIDTH - player2W;
 
 
-			//Ball
-			if( ballIsRun == 1 ){
+            //Ball
+            if( ballIsRun == 1 ){
 
-				LCD_SetTextColor( LCD_COLOR_BLACK );
-				LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
+                LCD_SetTextColor( LCD_COLOR_BLACK );
+                LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
 
-				//Touch wall
-				ballX += ballVX;
-				if( ballX <= 0 ){
-					ballX = 0;
-					ballVX *= -1;
-				}
-				else if( ballX + ballSize >= LCD_PIXEL_WIDTH ){
-					ballX = LCD_PIXEL_WIDTH - ballSize;
-					ballVX *= -1;
-				}
+                //Touch wall
+                ballX += ballVX;
+                if( ballX <= 0 ){
+                    ballX = 0;
+                    ballVX *= -1;
+                }
+                else if( ballX + ballSize >= LCD_PIXEL_WIDTH ){
+                    ballX = LCD_PIXEL_WIDTH - ballSize;
+                    ballVX *= -1;
+                }
 
-				//PONG!
-				ballY += ballVY;
-				if( ballY + ballSize >= player2Y ){
-					if( ballX + ballSize >= player2X && ballX <= player2X + player2W ){
-					if( ballX - ballSize <= player2Y + player2W/4 ){
-						ballVY =-3;
-						ballVX =-7;
-					}
-					else if( ballX >= player2Y + player2W - player2W/4 ){
-						ballVY =-3;
-						ballVX = 7;
-					}
-					else if( ballX + ballSize < player2Y + player2W/2 ){
-						ballVY =-7;
-						ballVX =-3;
-					}
-					else if( ballX > player2Y + player2W/2 ){
-						ballVY =-7;
-						ballVX = 3;
-					}
-					else{
-						ballVY =-9;
-						ballVX = 0;
-					}
-				}
-				else
-					BallReset();
-			}
+                //PONG!
+                ballY += ballVY;
+                if( ballY + ballSize >= player2Y ){
+                    if( ballX + ballSize >= player2X && ballX <= player2X + player2W ){
+                    if( ballX - ballSize <= player2Y + player2W/4 ){
+                        ballVY =-3;
+                        ballVX =-7;
+                    }
+                    else if( ballX >= player2Y + player2W - player2W/4 ){
+                        ballVY =-3;
+                        ballVX = 7;
+                    }
+                    else if( ballX + ballSize < player2Y + player2W/2 ){
+                        ballVY =-7;
+                        ballVX =-3;
+                    }
+                    else if( ballX > player2Y + player2W/2 ){
+                        ballVY =-7;
+                        ballVX = 3;
+                    }
+                    else{
+                        ballVY =-9;
+                        ballVX = 0;
+                    }
+                }
+                else
+                    BallReset();
+            }
 
-			if( ballY <= player1Y + player1H ){
-				if( ballX + ballSize >= player1X && ballX <= player1X + player1W ){
-					if( ballX - ballSize <= player1Y + player1W/4 ){
-						ballVY = 3;
-						ballVX =-7;
-					}
-					else if( ballX >= player1Y + player1W - player1W/4 ){
-						ballVY = 3;
-						ballVX = 7;
-					}
-					else if( ballX + ballSize < player1Y + player1W/2 ){
-						ballVY = 7;
-						ballVX =-3;
-					}
-					else if( ballX > player1Y + player1W/2 ){
-						ballVY = 7;
-						ballVX = 3;
-					}
-					else{
-						ballVY = 9;
-						ballVX = 0;
-					}
-				}
-				else
-					BallReset();
-			}
-		}
-	}
+            if( ballY <= player1Y + player1H ){
+                if( ballX + ballSize >= player1X && ballX <= player1X + player1W ){
+                    if( ballX - ballSize <= player1Y + player1W/4 ){
+                        ballVY = 3;
+                        ballVX =-7;
+                    }
+                    else if( ballX >= player1Y + player1W - player1W/4 ){
+                        ballVY = 3;
+                        ballVX = 7;
+                    }
+                    else if( ballX + ballSize < player1Y + player1W/2 ){
+                        ballVY = 7;
+                        ballVX =-3;
+                    }
+                    else if( ballX > player1Y + player1W/2 ){
+                        ballVY = 7;
+                        ballVX = 3;
+                    }
+                    else{
+                        ballVY = 9;
+                        ballVX = 0;
+                    }
+                }
+                else
+                    BallReset();
+            }
+        }
+    }
 }
 
-	void
+    void
 GAME_Render()
 {
-	LCD_SetTextColor( LCD_COLOR_WHITE );
-	LCD_DrawFullRect( player1X, player1Y, player1W, player1H );
-	LCD_DrawFullRect( player2X, player2Y, player2W, player2H );
-	LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
-	LCD_DrawLine( 10, LCD_PIXEL_HEIGHT / 2, LCD_PIXEL_WIDTH - 20, LCD_DIR_HORIZONTAL );
+    LCD_SetTextColor( LCD_COLOR_WHITE );
+    LCD_DrawFullRect( player1X, player1Y, player1W, player1H );
+    LCD_DrawFullRect( player2X, player2Y, player2W, player2H );
+    LCD_DrawFullRect( ballX, ballY, ballSize, ballSize );
+    LCD_DrawLine( 10, LCD_PIXEL_HEIGHT / 2, LCD_PIXEL_WIDTH - 20, LCD_DIR_HORIZONTAL );
 }
